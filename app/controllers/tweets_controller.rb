793efca20f3f23079802d41b6ebc89b7f2a02d25
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-26 14:27:12 -0500

    new index, new methods and html pages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,10 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweets.all
+    @tweets = Tweet.all
     render "index.html.erb"
   end
   def new
+    render "new.html.erb"
   end
   def create
   end
