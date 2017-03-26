0483dba8680f438c61589da99db0474dd1d7d3eb
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-26 16:07:48 -0500

    edit update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,7 @@ class TweetsController < ApplicationController
       user: "Laurie",
       tweet: params[:tweet]
       )
+    flash[:success] = "We did it! We all did it!"
     redirect_to "/tweets"
   end
   def show
@@ -18,9 +19,22 @@ class TweetsController < ApplicationController
     render "show.html.erb"
   end
   def edit
+    @tweet = Tweet.find_by(id: params[:id])
+    render "edit.html.erb"
   end
   def update
+    @tweet = Tweet.find_by(id: params[:id])
+    @tweet = Tweet.update(
+        user: "Laurie",
+        tweet: params[:tweet]
+      )
+    flash[:success] = "Tweet Successfully updated"
+    redirect_to "/"
   end
   def destroy
+    tweet = Tweet.finnd_by(id: params[:id])
+    tweet.destroy
+    flash[:warning] = "Tweet successfully demolished!"
+    redirect_to "/"
   end
 end
